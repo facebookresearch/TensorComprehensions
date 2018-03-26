d42bbf1dfb1a8f5d13fed58f383e0a7ffd64a2f5
Mingzhe Li <mingzhe0908@devgpu236.prn2.facebook.com>
2018-03-26 13:39:18 -0700

    Fix format issue

--- test/test_lang.cc
+++ test/test_lang.cc
@@ -29,10 +29,10 @@
 using namespace lang;
 
 #ifdef ROOT_PATH
-  const std::string expected_file_path =
-      std::string(ROOT_PATH) + "src/lang/test_expected/";
+const std::string expected_file_path =
+    std::string(ROOT_PATH) + "src/lang/test_expected/";
 #else
-  const std::string expected_file_path = "src/lang/test_expected/";
+const std::string expected_file_path = "src/lang/test_expected/";
 #endif
 
 static inline void barf(const char* fmt, ...) {
