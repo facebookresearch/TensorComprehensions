ca570da76b2e0bdab987318b5a32e639a8696bb4
prigoyal <priy2201@gmail.com>
2018-04-25 13:12:11 -0700

    Activate conda env which has TC build for test time

--- .jenkins/run_test.sh
+++ .jenkins/run_test.sh
@@ -10,6 +10,7 @@ source /etc/lsb-release
 
 if [[ $(conda --version | wc -c) -ne 0 ]]; then
   echo "Running TC PyTorch tests"
+  source activate tc-env
   ./test_python/run_test.sh
 else
   # TODO: modify 2LUT tests from example_MLP_model and enable on CI
