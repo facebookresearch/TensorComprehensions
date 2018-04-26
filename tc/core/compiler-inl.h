226797f250cf0193adcda4b164a943e6175ddda8
nicolasvasilache <nicolas.vasilache@gmail.com>
2018-04-26 09:03:17 -0600

    [C++ API] Step 2: add the new compiler files
    This PR adds the files implementing the new compiler API #322. This allows
    retiring the ExecutionEngine #309 which has been a significant source of
    unnecessary complexity.
    
    More specifically, this implements a more functional-style API for
    compilation. compile returns an owning pointer to a TcExecutor
    on which run, profile and uncheckedRun can be called.
    
    This also implements support for parse and inferOutputTensorInfo
    as well as operations on the parsed TreeRef in the detail namespace.
    
    Higher-level layers are responsible for caching compilation results.
    
    As part of the bigger refactoring, these files are not yet activated
    (or even compiled) therefore the changeset cannot be tested independently.
    This new implementation will be switched in as part of the last commit of the
    the global refactoring.

---
+++ tc/core/compiler-inl.h
@@ -0,0 +1,71 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "tc/core/compiler.h"
+
+#include <sstream>
+#include <string>
+
+#include "tc/core/flags.h"
+#include "tc/core/halide_utils.h"
+#include "tc/core/tensor.h"
+#include "tc/lang/canonicalize.h"
+
+namespace tc {
+namespace detail {
+void checkInputsCompliant(
+    const tc2halide::HalideComponents& halideComponents,
+    const std::vector<const DLConstTensor*>& inputsInfo);
+} // namespace detail
+
+template <typename Backend>
+std::unique_ptr<typename Backend::ExecutorType> compile(
+    const std::string& tc,
+    const std::string& entryPoint,
+    const std::vector<const DLConstTensor*>& inputs,
+    /* TODO: in the future also pass outputs for stride and alignment info */
+    const typename Backend::MappingOptionsType& options) {
+  auto parsedTcs = detail::parse(tc);
+  CHECK_EQ(parsedTcs.count(entryPoint), 1u)
+      << "attempting to access undefined function " << entryPoint;
+  return compile<Backend>(parsedTcs[entryPoint], inputs, options);
+}
+
+template <typename Backend>
+std::unique_ptr<typename Backend::ExecutorType> compile(
+    lang::TreeRef tcDefinition,
+    const std::vector<const DLConstTensor*>& inputs,
+    /* TODO: in the future also pass outputs for stride and alignment info */
+    const typename Backend::MappingOptionsType& options) {
+  using CompilationResultType = typename Backend::CompilationResultType;
+
+  auto inputsInfo = makeTensorInfoVector(inputs);
+  auto outputsInfo = detail::inferOutputTensorInfo(tcDefinition, inputs);
+  auto halideComponents =
+      tc2halide::translate(isl::with_exceptions::globalIslCtx(), tcDefinition);
+  detail::checkInputsCompliant(halideComponents, inputs);
+
+  auto tcName = lang::Def(tcDefinition).name().name();
+  CompilationResultType compilationResult = Backend::compileWithTcMapper(
+      tcName,
+      halideComponents,
+      inputs,
+      /* TODO outputs, */
+      options);
+  return std::unique_ptr<typename Backend::ExecutorType>(
+      new typename Backend::ExecutorType(
+          inputsInfo, outputsInfo, halideComponents, compilationResult));
+}
+} // namespace tc
---
+++ tc/core/compiler.cc
@@ -0,0 +1,92 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "tc/core/compiler.h"
+
+#include <sstream>
+#include <string>
+
+#include "tc/core/flags.h"
+#include "tc/core/halide_utils.h"
+#include "tc/core/tensor.h"
+#include "tc/lang/canonicalize.h"
+
+namespace tc {
+std::vector<TensorInfo> inferOutputTensorInfo(
+    const std::string& tc,
+    const std::string& entryPoint,
+    const std::vector<const DLConstTensor*> inputs) {
+  auto parsedTcs = detail::parse(tc);
+  CHECK_EQ(1, parsedTcs.count(entryPoint))
+      << "attempting to access undefined function " << entryPoint;
+  return tc::detail::inferOutputTensorInfo(parsedTcs[entryPoint], inputs);
+}
+
+namespace detail {
+void checkInputsCompliant(
+    const tc2halide::HalideComponents& halideComponents,
+    const std::vector<const DLConstTensor*>& inputsInfo) {
+  if (inputsInfo.size() != halideComponents.inputs.size()) {
+    throw lang::ErrorReport(halideComponents.getDef())
+        << "expected " << halideComponents.inputs.size() << " inputs but found "
+        << inputsInfo.size();
+  }
+  for (size_t i = 0; i < inputsInfo.size(); ++i) {
+    auto dlType = inputsInfo[i]->dtype;
+    auto hType = halideComponents.inputs[i].type();
+    // we have three type representations here: (1) halide Type (2) DLTensor
+    // type, and (3) the token representing the type in the frontend (e.g.
+    // TK_FLOAT) we need to translate to (3) to report user facing errors
+    auto dlLangType =
+        lang::TypeInfo(lang::TypeInfo::Code(dlType.code), dlType.bits)
+            .toScalarToken();
+    auto hLangType =
+        lang::TypeInfo(lang::TypeInfo::Code(hType.code()), hType.bits())
+            .toScalarToken();
+    if (dlLangType != hLangType) {
+      throw lang::ErrorReport(halideComponents.getDef().params()[i])
+          << "expected type " << lang::kindToString(hLangType) << " but found "
+          << lang::kindToString(dlLangType);
+    }
+    int hdim = halideComponents.inputs[i].dimensions();
+    int dldim = inputsInfo[i]->ndim;
+    if (dldim != hdim) {
+      throw lang::ErrorReport(halideComponents.getDef().params()[i])
+          << "expected a tensor with " << hdim << " dimensions but found "
+          << dldim << " dimensions.";
+    }
+  }
+}
+
+std::vector<TensorInfo> inferOutputTensorInfo(
+    lang::TreeRef tcDefinition,
+    const std::vector<const DLConstTensor*> inputs) {
+  return tc::inferOutputTensorInfo(
+      tc2halide::translate(isl::with_exceptions::globalIslCtx(), tcDefinition),
+      inputs);
+}
+
+std::map<std::string, lang::TreeRef> parse(const std::string& tc) {
+  lang::Parser parser(tc);
+  std::map<std::string, lang::TreeRef> parsed;
+  while (parser.L.cur().kind != lang::TK_EOF) {
+    auto t = parser.parseFunction();
+    auto name = lang::Def(t).name().name();
+    parsed.emplace(std::make_pair(name, t));
+  }
+  return parsed;
+}
+} // namespace detail
+} // namespace tc
