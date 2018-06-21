589ba9ed58e77fd841ca73a2df9a7ed3f4c76a16
Sven Verdoolaege <sven.verdoolaege@gmail.com>
2018-06-21 12:05:24 +0200

    promoteToRegistersBelow: use templated isl types

--- tc/core/polyhedral/cuda/memory_promotion_heuristic.cc
+++ tc/core/polyhedral/cuda/memory_promotion_heuristic.cc
@@ -654,8 +654,7 @@ void promoteToRegistersBelow(MappedScop& mscop, detail::ScheduleTree* scope) {
   auto blockSchedule = mscop.blockMappingSchedule(mscop.schedule());
 
   // Pure affine schedule without (mapping) filters.
-  isl::multi_union_pw_aff partialSchedMupa =
-      partialScheduleMupa<Prefix>(root, scope);
+  auto partialSchedMupa = partialScheduleMupa<Prefix>(root, scope);
   // Schedule with block mapping filter.
   auto partialSched =
       isl::union_map::from(partialSchedMupa).intersect_domain(blockMapping);
@@ -663,7 +662,7 @@ void promoteToRegistersBelow(MappedScop& mscop, detail::ScheduleTree* scope) {
   // performed with respect to the block mapping, so append the block schedule.
   // If the partial schedule contains it already, it will just end up with
   // identical dimensions without affecting the result of the checks.
-  partialSchedMupa = partialSchedMupa.flat_range_product(blockSchedule);
+  auto partialSchedBlockMupa = partialSchedMupa.range_product(blockSchedule);
 
   for (auto& tensorGroups : groupMap) {
     auto tensorId = tensorGroups.first;
@@ -677,11 +676,11 @@ void promoteToRegistersBelow(MappedScop& mscop, detail::ScheduleTree* scope) {
         continue;
       }
       if (!isPromotableToRegistersBelow(
-              *group, root, scope, partialSchedMupa, threadSchedule)) {
+              *group, root, scope, partialSchedBlockMupa, threadSchedule)) {
         continue;
       }
       // Check reuse within threads.
-      auto schedule = partialSchedMupa.flat_range_product(threadSchedule);
+      auto schedule = partialSchedBlockMupa.range_product(threadSchedule);
       if (!hasReuseWithin(*group, schedule)) {
         continue;
       }
