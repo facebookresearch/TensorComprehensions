8eebd60df9a7aa280136c9e5baf143b9bc53ff24
Jules Pondard <artix18@fb.com>
2018-06-26 15:44:30 -0400

    add file to check best genetic algorithm options

---
+++ tc/examples/optim_gen.py
@@ -0,0 +1,43 @@
+import numpy as np
+import ipdb
+import torch
+import torch.optim as optim
+import torch.nn as nn
+import torch.utils.data
+import torch.nn.functional as F
+import ipdb
+from itertools import count
+from collections import namedtuple
+from torch.distributions import Categorical
+import tensor_comprehensions as tc
+#from visdom import Visdom
+from collections import deque
+
+import my_utils
+
+code = """
+def group_normalization(
+    float(N, G, D, H, W) I, float(G, D) gamma, float(G, D) beta)
+    -> (O, mean, var)
+{
+    mean(n, g) +=! I(n, g, r_d, r_h, r_w)
+     var(n, g) +=! I(n, g, r_d, r_h, r_w) * I(n, g, r_d, r_h, r_w)
+
+    O(n, g, d, h, w) = gamma(g, d)
+      * ( I(n, g, d, h, w) - mean(n, g) * 4 )
+      * rsqrt( var(n, g) * 4
+            - mean(n, g) * mean(n, g) * 4 * 4
+            + 1e-5)
+      + beta(g, d)
+}
+"""
+
+N, G, D, H, W = 5, 5, 5, 5, 5
+I, gamma, beta = torch.randn(N, G, D, H, W).cuda(), torch.randn(G, D).cuda(), torch.randn(G, D).cuda()
+
+tc_prog = tc.define(code, name="group_normalization")
+cache = "my_file.txt"
+tc_prog.autotune(I, gamma, beta, **tc.autotuner_settings, cache=cache)
+print(tc.decode(cache + ".options"))
+
+
