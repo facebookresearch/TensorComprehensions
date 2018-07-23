20b411c5d2574248e2b3d2b8cefc5c3739a22438
Jules Pondard <artix18@fb.com>
2018-07-23 17:43:18 +0200

    Normalized everything for PR prep

--- tc/examples/predict_time.py
+++ tc/examples/predict_time.py
@@ -27,20 +27,20 @@ def getRandom():
         opt_v[i] = np.random.randint(my_utils.cat_sz[i])
     return opt_v
 
-def makeDataset(tc_prog, inp):
+def makeDataset():
     sz = 500
     datasetX, datasetY = [], []
     for _ in range(sz):
         opt = getRandom()
-        yi = createY(x)
+        yi = createY(opt)
         datasetX.append(opt)
         datasetY.append(yi)
     #with Pool(sz) as p:
     #    datasetY = p.starmap(createY, datasetX)
     return np.array(datasetX), np.array(datasetY)
 
-def learn(tc_prog, inp):
-    datasetX, datasetY = makeDataset(tc_prog, inp)
+def learn():
+    datasetX, datasetY = makeDataset()
     print(min(datasetY))
     Xtrain, Xtest, Ytrain, Ytest = train_test_split(datasetX, datasetY, test_size=0.2, random_state = 42)
     model1 = GradientBoostingRegressor(n_estimators=1000)
@@ -54,4 +54,4 @@ def learn(tc_prog, inp):
     print(np.corrcoef(pred0, Ytrain)[0, 1]**2)
     print(np.corrcoef(pred1, Ytest)[0,1]**2)
 
-learn(gn, inp)
+learn()
