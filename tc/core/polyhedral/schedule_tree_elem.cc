71fb1170e544fe6162bdd0c6f0e8f82869954def
Sven Verdoolaege <sven.verdoolaege@gmail.com>
2018-07-25 17:34:55 +0200

    ScheduleTreeBand::memberRange: do not call domain() on a set space
    The space of an isl_multi_union_pw_aff is the common range space,
    i.e., a set space.  The current version of isl tolerates calling
    domain() on a set space, but it is technically incorrect.
    Call params() instead.
    Moving to templated isl types will make this issue more evident.

--- tc/core/polyhedral/schedule_tree_elem.cc
+++ tc/core/polyhedral/schedule_tree_elem.cc
@@ -253,7 +253,7 @@ void ScheduleTreeBand::drop(size_t pos, size_t n) {
 isl::multi_union_pw_aff ScheduleTreeBand::memberRange(size_t first, size_t n)
     const {
   auto list = mupa_.get_union_pw_aff_list();
-  auto space = mupa_.get_space().domain().add_unnamed_tuple_ui(n);
+  auto space = mupa_.get_space().params().add_unnamed_tuple_ui(n);
   auto end = first + n;
   TC_CHECK_LE(end, nMember());
   list = list.drop(end, nMember() - end);
